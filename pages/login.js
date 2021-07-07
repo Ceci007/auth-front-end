2dad7c89b36d4081723c312efcea6bba72b13a0e
Ceci007 <cecibenitezca@gmail.com>
2021-07-07 10:27:59 -0400

    Added login form

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,66 @@
+import { useState } from 'react'
+import Link from 'next/link'
+import { toast } from 'react-toastify';
+import axios from 'axios'
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      setLoading(true);
+      const { data } = await axios.post(`/api/login`, {
+        email,
+        password
+      });
+      
+      toast.success('Login successful');
+      console.log('Login data', data);
+
+      setEmail('');
+      setPassword('');
+      setLoading(false);
+    } catch(err) {
+      toast.error(err.response.data);
+      setLoading(false);
+    }
+  }
+
   return (
-    <div className="header">
-      <h1>Login</h1>
-      <p>from next.js...</p>
+    <div>
+      <form className="form-control" onSubmit={handleSubmit}>
+        <input 
+          type="email" 
+          className="form-control__input" 
+          value={email} 
+          onChange={e => setEmail(e.target.value)} 
+          placeholder="Enter email"
+          required
+        />
+        <input 
+          type="password" 
+          className="form-control__input" 
+          value={password} 
+          onChange={e => setPassword(e.target.value)} 
+          placeholder="Enter password"
+          required
+        />
+        <button 
+          type="submit" 
+          className="btn btn--primary"
+          disabled={!email || !password || loading}
+        >
+          { loading ? 
+            <CircularProgress style={{color: '#fff', width: '15px', height: '15px'}} /> : 
+            'Login' }
+        </button>
+        <p className="text-center">Don't have an account? <Link href="/register"><a className="link">Signup</a></Link></p>
+      </form>
     </div>
   );
 }
